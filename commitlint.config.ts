caca492857e4f3be03452a848c133f86b1ec47eb
María Fernández <matefe1706@gmail.com>
2025-10-15 14:14:16 +0200

    chore(core): add scopes to commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,38 @@ import type { UserConfig } from '@commitlint/types'
 
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'assets',
+        'components',
+        'composables',
+        'config',
+        'constants',
+        'core',
+        'dependencies',
+        'documentation',
+        'interfaces',
+        'layout',
+        'locales',
+        'middlewares',
+        'mocks',
+        'pages',
+        'plugins',
+        'public',
+        'readme',
+        'server',
+        'services',
+        'stores',
+        'styles',
+        'testing',
+        'types',
+        'utils',
+      ],
+    ],
+  },
 }
 
 export default config
