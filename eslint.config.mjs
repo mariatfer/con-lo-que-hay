ba85b1f5fa1b74ea042854600a96a0c8dca24a7d
María Fernández <matefe1706@gmail.com>
2025-10-15 13:56:16 +0200

    chore: first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default withNuxt({
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-multiple-template-root': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': ['error'],
+    'vue/no-v-html': 'off',
+  },
+  plugins: {
+    prettier: prettierPlugin,
+  },
+})
